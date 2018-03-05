5b7132f4e581bdbd0053aac183ab8656c5ff5b48
barestu <fheartnet@gmail.com>
2018-03-05 17:33:10 +0700

    Soal Roman Numerals Recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,30 @@
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
+  var romanArray = [
+      [1000, 'M'],
+      [500, 'D'],
+      [400, 'CD'],
+      [100, 'C'],
+      [50, 'L'],
+      [40, 'XL'],
+      [10, 'X'],
+      [9, 'IX'],
+      [5, 'V'],
+      [4, 'IV'],
+      [1, 'I']
+    ];
+
+  if (input === 0) {
+    return '';
+  } else {
+    for (var i = 0; i < romanArray.length; i++) {
+      if (input >= romanArray[i][0]) {
+        input -= romanArray[i][0];
+        return romanArray[i][1] + toRoman(input);
+      }
+    }
+  }
+
 }
 
 console.log('My totally sweet testing script for new roman\n')
