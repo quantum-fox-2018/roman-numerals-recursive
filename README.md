e3f2e350d0d09fef696e6704294d8681724d93a8
yasirmaulana <yasir.maulana@gmail.com>
2018-03-05 17:59:59 +0700

    day 1, roman-numerals-recursive

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # roman-numerals-recursive
+# inputnya diatas 3000 masih erorr
+INISIALISASI roman = ''; num = input
+IF num LEBIHKECIL 1
+  RETURN roman
+
+LOOPING SELAMA num>=1000
+  roman += 'M'
+  num -= 1000
+
+IF num LEBIHBESAR 500
+  IF num LEBIHBESAR 900
+    roman += 'CM'
+    num -= 900
+  ELSE
+    roman += 'D'
+    num -= 500
+
+LOOPING SELAMA num>=100
+  IF num LEBIHBESAR 400
+    roman += 'CD'
+    num -= 400
+  ELSE
+    roman += 'C'
+    num -= 100
+
+IF num LEBIHBESAR 50
+  IF num LEBIHBESAR 90
+    roman += 'XC'
+    num -= 90
+  ELSE
+    roman += 'L'
+    num -= 50
+
+LOOPING SELAMA num LEBIHBESAR 10
+  IF num LEBIHBESAR 40
+    roman += 'XL'
+    num -= 40
+  ELSE
+    roman += 'X'
+    num -= 10
+
+IF num LEBIHBESAR 5
+  IF num LEBIHBESAR 9
+    roman += 'IX'
+    num -= 9
+  ELSE
+    roman += 'V'
+    num -= 5
+
+LOOPING SELAMA num LEBIHBESAR 1
+  IF num LEBIHBESAR 4
+    roman += 'IV'
+    num -= 4
+  ELSE
+    roman += 'I'
+    num -= 1
+
+RETURN roman + toRoman(num);
--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,8 +1,84 @@
 function toRoman(input) {
-  // start your code here
-  return to_roman(input);
+  var roman = ''; num = input
+  if(num<1){
+    return roman
+  }
+
+  while(num>=1000){
+    roman += 'M'
+    num -= 1000
+  }
+
+  if(num>=500){
+    if(num>=900){
+      roman += 'CM'
+      num -= 900
+    }
+    else {
+      roman += 'D'
+      num -= 500
+    }
+  }
+
+  while(num>=100){
+    if(num>=400){
+      roman += 'CD'
+      num -= 400
+    }
+    else {
+      roman += 'C'
+      num -= 100
+    }
+  }
+
+  if(num>=50){
+    if(num>=90){
+      roman += 'XC'
+      num -= 90
+    }
+    else {
+      roman += 'L'
+      num -= 50
+    }
+  }
+
+  while(num>=10){
+    if(num>=40){
+      roman += 'XL'
+      num -= 40
+    }
+    else {
+      roman += 'X'
+      num -= 10
+    }
+  }
+
+  if(num>=5){
+    if(num>=9){
+      roman += 'IX'
+      num -= 9
+    }
+    else {
+      roman += 'V'
+      num -= 5
+    }
+  }
+
+  while(num>=1){
+    if(num>=4){
+      roman += 'IV'
+      num -= 4
+    }
+    else {
+      roman += 'I'
+      num -= 1
+    }
+  }
+
+  return roman + toRoman(num);
 }
 
+// console.log(toRoman(5300))
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|————————')
@@ -11,5 +87,3 @@ console.log('9     | IX       | ', toRoman(9))
 console.log('23    | XXIII    | ', toRoman(23))
 console.log('1453  | MCDLIII  | ', toRoman(1453))
 console.log('1646  | MDCXLVI  | ', toRoman(1646))
-
-module.exports = toRoman
