fe1fbcfb695fd4bd392cf0d1bd095b84093b17cd
Fitrul Islam <fitrul.islam@gmail.com>
2018-03-05 14:45:58 +0700

    done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,20 @@
 function toRoman(input) {
-  // start your code here
-  return to_roman(input);
+  var numRom = [{num:1000,rom:'M'},{num:900,rom:'CM'},{num:500,rom:'D'},{num:400,rom:'CD'},
+                {num:100,rom:'C'},{num:90,rom:'XC'},{num:50,rom:'L'},{num:40,rom:'XL'},
+                {num:10,rom:'X'},{num:9,rom:'IX'},{num:5,rom:'V'},{num:4,rom:'IV'},
+                {num:1,rom:'I'}];
+  var roman = '';
+  if(input==0){
+    return roman;
+  } else {
+    for(let i=0;i<numRom.length;i++){
+      if(input>=numRom[i].num){
+        let hasil = input - numRom[i].num;
+        roman += numRom[i].rom;
+        return roman + toRoman(hasil)
+      }
+    }
+  }
 }
 
 console.log('My totally sweet testing script for new roman\n')
