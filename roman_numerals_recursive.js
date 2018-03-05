e809809e288705eb397bac3b1247ec4c10b5deb5
annisaayu <annisaayu569@gmail.com>
2018-03-05 18:36:53 +0700

    tugas roman rekursif

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,19 @@
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
+  if(input < 1){
+    return '';
+  }
+  let arrNum = [['I', 1],['IV', 4],['V', 5],
+                ['IX', 9], ['X', 10],['XL', 40],
+                ['L', 50], ['XC', 90], ['C', 100],
+                ['CD', 400], ['D', 500],['CM', 900], ['M', 1000]];
+
+  for(let i= arrNum.length-1; i>=0; i--){
+    if(input >= arrNum[i][1]){
+      return arrNum[i][0] + toRoman(input - arrNum[i][1])
+    }
+  }
+
 }
 
 console.log('My totally sweet testing script for new roman\n')
