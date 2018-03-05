4ad8a5aa2b4a47838e9129ddb7f337abcce68a66
faizal <faizalramanda@gmail.com>
2018-03-05 22:14:19 +0700

    roman numeral recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,33 @@
+/*
+SET variable library dengan value array dengan value = [[1000, 'M'],[900, 'CM'],[500, 'D'],[400, 'CD'],[100, 'C'],[90, 'XC'],
+[50, 'L'],[40, 'XL'],[10, 'X'],[9, 'IX'],[5, 'V'],[4, 'IV'],[1, 'I']]
+
+CREATE function toRoman dengan parameter sebuah angka bernama input
+
+  IF input sama dengan 0
+     maka kembalikan string kosong
+  ELSE
+    DO looping dari nilai i = 0 hingga panjang library, setiap iterasi i+1
+      IF input masih lebih besar sama dengan dari library i index 0 maka
+        kembalikan nilai library i index 1 ditambah dengan fungsi toRoman dengan parameter input yang telah dikurangi library i index 0
+*/
+
+
+var library = [[1000, 'M'],[900, 'CM'],[500, 'D'],[400, 'CD'],[100, 'C'],[90, 'XC'],
+[50, 'L'],[40, 'XL'],[10, 'X'],[9, 'IX'],[5, 'V'],[4, 'IV'],[1, 'I']]
+
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
+  if (input === 0) {
+    return ''
+  } else {
+    for (var i = 0; i < library.length; i++) {
+      if (input >= library[i][0]) {
+        return library[i][1] + toRoman(input - library[i][0])
+      }
+    }
+  }
+
 }
 
 console.log('My totally sweet testing script for new roman\n')
