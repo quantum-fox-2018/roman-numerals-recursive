6987cf102fb461b345918acf2f6e3c9f487a3f2d
Fitrul Islam <fitrul.islam@gmail.com>
2018-03-05 19:48:44 +0700

    done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,8 +1,43 @@
 function toRoman(input) {
-  var numRom = [{num:1000,rom:'M'},{num:900,rom:'CM'},{num:500,rom:'D'},{num:400,rom:'CD'},
-                {num:100,rom:'C'},{num:90,rom:'XC'},{num:50,rom:'L'},{num:40,rom:'XL'},
-                {num:10,rom:'X'},{num:9,rom:'IX'},{num:5,rom:'V'},{num:4,rom:'IV'},
-                {num:1,rom:'I'}];
+  var numRom = [{
+    num:1000,
+    rom:'M'
+  },{
+    num:900,
+    rom:'CM'
+  },{
+    num:500,
+    rom:'D'
+  },{
+    num:400,
+    rom:'CD'
+  },{num:100,
+    rom:'C'
+  },{
+    num:90,
+    rom:'XC'
+  },{
+    num:50,
+    rom:'L'
+  },{
+    num:40,
+    rom:'XL'
+  },{
+    num:10,
+    rom:'X'
+  },{
+    num:9,
+    rom:'IX'
+  },{
+    num:5,
+    rom:'V'
+  },{
+    num:4,
+    rom:'IV'
+  },{
+    num:1,
+    rom:'I'
+  }];
   var roman = '';
   if(input==0){
     return roman;
