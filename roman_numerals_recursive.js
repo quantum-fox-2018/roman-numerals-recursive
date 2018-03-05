4aaae988e2da35057a26b6deb76fc622596a1236
cantdocpp <cihuyman95@gmail.com>
2018-03-05 19:39:48 +0700

    roman numerals recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,18 @@
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
+  var angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+  var angkaRomawi = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM', 'M'];
+  var string = '';
+  if (input == 0) {
+    return '';
+  } else {
+    for (var i = angka.length; i >= 0; i--) {
+      if (input >= angka[i]) {
+        string += angkaRomawi[i]
+        return angkaRomawi[i] + toRoman(input - angka[i])
+      }
+    }
+  }
 }
 
 console.log('My totally sweet testing script for new roman\n')
@@ -12,4 +24,4 @@ console.log('23    | XXIII    | ', toRoman(23))
 console.log('1453  | MCDLIII  | ', toRoman(1453))
 console.log('1646  | MDCXLVI  | ', toRoman(1646))
 
-module.exports = toRoman
+// module.exports = toRoman
